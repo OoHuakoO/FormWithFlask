33151b8c7523bb401930b2b61ea647d49939fbaf
khomsark <komsark_@hotmail.co.th>
2020-08-21 16:16:24 +0700

    commit

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask , render_template
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,7 +6,7 @@ def hello():
     return render_template('index.html')
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     app.run(debug=True)
 
 
