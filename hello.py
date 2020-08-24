e412f1473e0447d12b82aede8fc12bb1768846a9
khomsark <komsark_@hotmail.co.th>
2020-08-24 21:35:04 +0700

    add

--- hello.py
+++ hello.py
@@ -4,8 +4,11 @@ app = Flask(__name__)
 con = pymysql.connect('localhost','root','','studentdb')
 @app.route("/")
 def hello():
-    age = "python"
-    return render_template('index.html',data=age)
+    with con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM student")
+        rows = cur.fetchAll()
+        return render_template('index.html',data=age)
 
 if __name__ == "__main__":
     app.run(debug=True)
