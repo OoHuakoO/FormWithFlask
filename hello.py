019bf6a463cec916c0bb68e2885bbc86215a2a0b
khomsark <komsark_@hotmail.co.th>
2020-08-25 17:52:12 +0700

    finish

--- hello.py
+++ hello.py
@@ -33,6 +33,20 @@ def delete(id_data):
          conn.commit()
          return redirect(url_for('showData'))
 
+
+@app.route("/update",methods=['POST'])
+def update():
+    if request.method=='POST':
+        id_update = request.form['id']
+        fname = request.form['fname']
+        lname = request.form['lname']
+        phone = request.form['phone']
+        with conn.cursor() as cursor:
+            sql = "update student set fname=%s , lname=%s, phone=%s where id = %s "
+            cursor.execute(sql,(fname,lname,phone,id_update))
+            conn.commit()
+            return redirect(url_for('showData'))
+
 if __name__ == "__main__":
     app.run(debug=True)
     
